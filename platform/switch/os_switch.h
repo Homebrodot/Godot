b03a7cb2cd2907cdf55ba79eb19be5aff16a433d
Caroline Joy Bell <halotroop2288@proton.me>
2024-01-27 16:45:34 -0600

    Revert "[Switch] Create Switch API for virtual keyboards"
    This reverts commit 0362e3f8b21f0ded27c49004fa89cfb57576ae13.

--- platform/switch/os_switch.h
+++ platform/switch/os_switch.h
@@ -28,7 +28,6 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#include "api/switch_singleton.h"
 #include "context_gl_switch_egl.h"
 #include "core/os/input.h"
 #include "core/os/os.h"
